9e702edf3fba6ec7962b0e468ac3944fe5845e62
shadatshohan <shadatshohan5@gmail.com>
2021-09-09 17:15:29 +0600

    logic_slider

--- artfort/ecommerce/views.py
+++ artfort/ecommerce/views.py
@@ -8,9 +8,12 @@ def home(request):
 	allProd=[]
 	catprod=Product.objects.values('id','category')
 	cats={item['category'] for item in catprod}
-	for cat in cats:
+	cats=list(cats)
+	cats.insert(0,'upper')
+	catsnew=list(dict.fromkeys(cats))
+	print(catsnew)
+	for cat in catsnew:
 		prod=Product.objects.filter(category=cat)
-		print(prod)
 		n=len(prod)
 		nSlides=(n//4)+ceil((n/4)-(n//4))
 		allProd.append([prod,range(1,nSlides),nSlides])
